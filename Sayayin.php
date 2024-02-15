3c7aad2800afe888e5c8cf35b5fdb629298e57e0
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-14 22:54:36 -0500

    InstanceClass

--- Sayayin.php
+++ Sayayin.php
@@ -2,14 +2,23 @@
 
 // CreandoLaClase
 class Saiyajin{
-
     //Atributos
     public string $nombre="Goku";
     public int $nivel_pelea=1000;
-    
-
-
-
-
+  
+    //Metodo1
+    public function Saludar():string{
+        return "Hola Soy " . $this->nombre;
+    }
+    //Metodo2
+    public function Nivel_De_Pelea(){
+        return "Hola Soy " . $this->nombre . " Mi Nivel De Pelea Es: " . $this->nivel_pelea;
+    }
 }
+
+
+
+//InstanciaDeClase
+$goku = new Saiyajin();
+
 ?>
