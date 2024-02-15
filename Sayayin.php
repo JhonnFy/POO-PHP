7d77df6735135b0d5762ebcfc5cde6f9e54bfbbb
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-15 00:25:06 -0500

    Constructor

--- Sayayin.php
+++ Sayayin.php
@@ -1,25 +1,39 @@
 <?php
 
-    class Saiyajin{
+//CreateClass
+class Saiyajin{
+    //Atributes
+    public string $nombre;
+    public int $nivel_pelea;
 
-        public string $saludar;
-        public int $nivel_pelea;
-
-        public function saludar($saludar){
-               return $this->saludar = $saludar;
-        }
-
-        public function nivel_pelea($nivel_pelea){
-            return $this->nivel_pelea = $nivel_pelea;
-        }
-
-        public function __construct($saludar, $nivel_pelea){
-            $this->saludar = $saludar;
-            $this->nivel_pelea = $nivel_pelea;
-        }
-
+    public function __construct($nombre, $nivel_pelea){
+        $this->nombre=$nombre;
+        $this->nivel_pelea=$nivel_pelea;
+    }
+      
+    //Method1
+    public function Saludar($saludar="Hola Soy"):string{
+        return $saludar . $this->nombre;
+    }
+    //Method2
+    public function Nivel_De_Pelea($nivel_pelea="Mi Nivel De Pelea Es:"){
+        return $nivel_pelea .  $this->nivel_pelea;
     }
 
-    $goku = new Saiyajin();
+    //Method3
+    public function Characteristcs(){
+        return $this->Saludar() . " & " . $this->Nivel_De_Pelea();
+    }
+
+}
+
+//InstanciaDeClase
+$goku = new Saiyajin("Goku",1000);
+echo $goku->Characteristcs();
+
+echo "<br><br>";
+
+$vegueta = new Saiyajin("Vegueta",100);
+echo $vegueta->Characteristcs();
 
 ?>
